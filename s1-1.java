9b6736b0738715d389b0b5b4734017f3a03d3f49
Brandon <brandon49623@gmail.com>
2019-04-11 12:48:12 -0700

    asdfasdf

---
+++ s1-1.java
@@ -0,0 +1,19 @@
+//Joe Kim
+//Program2.java
+//8.21.17
+
+public class Program2  //class is Program2
+{ public static void main ( String [] args )
+    { 
+  Program2 P2 = new Program2();
+  P2.PrintStuff();
+ }
+ 
+ public void PrintStuff()
+ {
+  System.out.println("\n\n\n");
+  System.out.println("Hello. My name is Joe Kim 2.");
+  System.out.print("\n\n\n");
+ }
+}
+
